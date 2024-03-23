7105a07ce655262cfea5501c5da2896f44f11bec
Samarth Kamble <kamble.samarth91@gmail.com>
2024-03-23 14:56:47 +0530

    Created a user model

---
+++ models/user.model.ts
@@ -0,0 +1,82 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (enteredPassword: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "Please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Your password must be longer than 6 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//Compare Password
+
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<Boolean> {
+  return await bcrypt.compare(enteredPassword, this.Password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+
+export default userModel;
